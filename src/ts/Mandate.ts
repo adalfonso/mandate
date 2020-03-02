b0a4133c59b9d3ed663be3f7081e5023e82d887f
Anthony Dalfonso <anthonyjdalfonso@gmail.com>
2020-03-01 20:00:35 -0500

    Add basic date formatting

--- src/ts/Mandate.ts
+++ src/ts/Mandate.ts
@@ -1,11 +1,58 @@
-export default class Mandate {
-  private _date: Date;
+const months = [
+    "January",
+    "February",
+    "March",
+    "April",
+    "May",
+    "June",
+    "July",
+    "August",
+    "September",
+    "October",
+    "November",
+    "December"
+];
 
-  constructor(date: Date | string) {
-    if (typeof date === "string") {
-      this._date = new Date(date);
-    } else {
-      this._date = date;
+const formatInputs = {
+    "Y{4}": date => date.getFullYear().toString(),
+
+    "Y{2,3}": date =>
+        date
+            .getFullYear()
+            .toString()
+            .slice(-2),
+
+    "M{4}": date => months[date.getMonth()],
+
+    "M{3}": date => months[date.getMonth()].slice(0, 3),
+
+    "M{1,2}(?!a)": date => date.getMonth() + 1,
+
+    "D{2}(?!e)": date =>
+        date.getDate() < 10 ? "0" + date.getDate() : date.getDate(),
+
+    "D(?!e)": date => date.getDate()
+};
+
+export default class Mandate {
+    private _date: Date;
+
+    constructor(date: Date | string) {
+        if (typeof date === "string") {
+            this._date = new Date(date);
+        } else {
+            this._date = date;
+        }
     }
-  }
+
+    public format(input: string): string {
+        for (let parseInput in formatInputs) {
+            let regex = new RegExp(parseInput, "g");
+            let closure = formatInputs[parseInput];
+
+            input = input.replace(regex, closure(this._date));
+        }
+
+        return input;
+    }
 }
--- tests/unit/mocha/Mandate.test.ts
+++ tests/unit/mocha/Mandate.test.ts
@@ -2,9 +2,81 @@ import { expect } from "chai";
 import Sut from "../../../src/ts/Mandate";
 
 describe("Mandate", function() {
-  it("creates a new instance", function() {
-    const sut = new Sut(new Date());
+    it("creates a new instance", function() {
+        const sut = new Sut(new Date());
 
-    expect(true).equal(true);
-  });
+        expect(true).equal(true);
+    });
+
+    describe("basic format", function() {
+        it("formats full year", function() {
+            const sut = new Sut(new Date(2020, 1, 1));
+
+            expect(sut.format("YYYY")).equal("2020");
+        });
+
+        it("formats short year", function() {
+            const sut = new Sut(new Date(2020, 1, 1));
+
+            expect(sut.format("YYY")).equal("20");
+            expect(sut.format("YY")).equal("20");
+        });
+
+        it("formats alpha month", function() {
+            const sut = new Sut(new Date(2020, 9, 1));
+
+            expect(sut.format("MMMM")).equal("October");
+            expect(sut.format("MMM")).equal("Oct");
+        });
+
+        it("formats numeric month", function() {
+            const sut = new Sut(new Date(2020, 4, 1));
+
+            expect(sut.format("MM")).equal("5");
+            expect(sut.format("M")).equal("5");
+        });
+
+        it("formats day", function() {
+            const sut = new Sut(new Date(2020, 4, 8));
+
+            expect(sut.format("DD")).equal("08");
+            expect(sut.format("D")).equal("8");
+        });
+
+        it("doesn't format incorrect input", function() {
+            const sut = new Sut(new Date(2020, 1, 1));
+
+            expect(sut.format("G")).equal("G");
+        });
+    });
+
+    describe("complex format", function() {
+        it("formats long year and short year", function() {
+            const sut = new Sut(new Date(2020, 1, 1));
+
+            expect(sut.format("YYYYYY")).equal("202020");
+        });
+
+        it("formats two long years", function() {
+            const sut = new Sut(new Date(2020, 1, 1));
+
+            expect(sut.format("YYYYYYYY")).equal("20202020");
+        });
+
+        it("formats full date", function() {
+            const sut = new Sut(new Date(1989, 2, 22));
+
+            expect(sut.format("MMMM DD, YYYY")).equal("March 22, 1989");
+        });
+    });
+
+    describe("edge cases", function() {
+        it("formats full date with words", function() {
+            const sut = new Sut(new Date(1989, 2, 22));
+
+            expect(sut.format("MMMM DD, YYYYhello")).equal(
+                "March 22, 1989hello"
+            );
+        });
+    });
 });
