2f0db05c63c66c30a2f785a5b8bd9533d945e5de
Anthony Dalfonso <anthony@anthonydalfonso.com>
2020-03-28 18:04:20 -0400

    Clean up unit tests

--- tests/unit/mocha/Mandate.test.ts
+++ tests/unit/mocha/Mandate.test.ts
@@ -164,123 +164,128 @@ describe("Mandate", () => {
   });
 
   describe("format", () => {
-    describe("basic format", () => {
-      it("formats full year", () => {
+    describe("basic", () => {
+      it("year (full)", () => {
         const sut = Sut(new Date(2020, 1, 1));
 
         expect(sut.format("YYYY")).equal("2020");
       });
 
-      it("formats short year", () => {
+      it("year (short)", () => {
         const sut = Sut(new Date(2020, 1, 1));
 
         expect(sut.format("YY")).equal("20");
       });
 
-      it("formats alpha month", () => {
-        const sut = Sut(new Date(2020, 9, 1));
+      it("month (alpha)", () => {
+        [
+          ["January", "Jan"],
+          ["February", "Feb"],
+          ["March", "Mar"],
+          ["April", "Apr"],
+          ["May", "May"],
+          ["June", "Jun"],
+          ["July", "Jul"],
+          ["August", "Aug"],
+          ["September", "Sep"],
+          ["October", "Oct"],
+          ["November", "Nov"],
+          ["December", "Dec"]
+        ].forEach((output, input) => {
+          const sut = Sut(new Date(2020, input, 1));
 
-        expect(sut.format("MMMM")).equal("October");
-        expect(sut.format("MMM")).equal("Oct");
+          expect(sut.format("MMMM")).equal(output[0]);
+          expect(sut.format("MMM")).equal(output[1]);
+        });
       });
 
-      it("formats numeric month", () => {
-        const sut = Sut(new Date(2020, 4, 1));
+      it("month (numeric)", () => {
+        [
+          [new Date(2020, 0, 1), "01", "1"],
+          [new Date(2020, 9, 1), "10", "10"]
+        ].forEach(data => {
+          const sut = Sut(data[0]);
 
-        expect(sut.format("MM")).equal("05");
-        expect(sut.format("M")).equal("5");
-
-        const sut2 = Sut(new Date(2020, 10, 1));
-
-        expect(sut2.format("MM")).equal("11");
-        expect(sut2.format("M")).equal("11");
+          expect(sut.format("MM")).equal(data[1]);
+          expect(sut.format("M")).equal(data[2]);
+        });
       });
 
-      it("formats day", () => {
-        const sut = Sut(new Date(2020, 4, 8));
+      it("day", () => {
+        [
+          [new Date(2020, 4, 1), "01", "1"],
+          [new Date(2020, 4, 10), "10", "10"]
+        ].forEach(data => {
+          const sut = Sut(data[0]);
 
-        expect(sut.format("DD")).equal("08");
-        expect(sut.format("D")).equal("8");
-
-        const sut2 = Sut(new Date(2020, 4, 15));
-
-        expect(sut2.format("DD")).equal("15");
-        expect(sut2.format("D")).equal("15");
+          expect(sut.format("DD")).equal(data[1]);
+          expect(sut.format("D")).equal(data[2]);
+        });
       });
 
-      it("formats 24 hours", () => {
-        const sut = Sut(new Date(2020, 4, 8, 4, 5, 3));
+      it("24-hour", () => {
+        [
+          [new Date(2020, 4, 8, 4, 5, 3), "04", "4"],
+          [new Date(2020, 4, 8, 0, 5, 3), "00", "0"],
+          [new Date(2020, 4, 8, 13, 5, 3), "13", "13"]
+        ].forEach(data => {
+          const sut = Sut(data[0]);
 
-        expect(sut.format("HH")).equal("04");
-        expect(sut.format("H")).equal("4");
-
-        const sut2 = Sut(new Date(2020, 4, 8, 0, 5, 3));
-
-        expect(sut2.format("HH")).equal("00");
-        expect(sut2.format("H")).equal("0");
-
-        const sut3 = Sut(new Date(2020, 4, 8, 13, 5, 3));
-
-        expect(sut3.format("HH")).equal("13");
-        expect(sut3.format("H")).equal("13");
+          expect(sut.format("HH")).equal(data[1]);
+          expect(sut.format("H")).equal(data[2]);
+        });
       });
 
-      it("formats 12 hours", () => {
-        const sut = Sut(new Date(2020, 4, 8, 4, 5, 3));
+      it("12-hour", () => {
+        [
+          [new Date(2020, 4, 8, 4, 5, 3), "04", "4"],
+          [new Date(2020, 4, 8, 0, 5, 3), "12", "12"],
+          [new Date(2020, 4, 8, 13, 5, 3), "01", "1"]
+        ].forEach(data => {
+          const sut = Sut(data[0]);
 
-        expect(sut.format("hh")).equal("04");
-        expect(sut.format("h")).equal("4");
-
-        const sut2 = Sut(new Date(2020, 4, 8, 0, 5, 3));
-
-        expect(sut2.format("hh")).equal("12");
-        expect(sut2.format("h")).equal("12");
-
-        const sut3 = Sut(new Date(2020, 4, 8, 13, 5, 3));
-
-        expect(sut3.format("hh")).equal("01");
-        expect(sut3.format("h")).equal("1");
+          expect(sut.format("hh")).equal(data[1]);
+          expect(sut.format("h")).equal(data[2]);
+        });
       });
 
-      it("formats ante and post meridiem", () => {
-        const sut = Sut(new Date(2020, 4, 8, 4, 5, 3));
+      it("am/pm", () => {
+        [
+          [new Date(2020, 4, 8, 4, 5, 3), "am", "AM"],
+          [new Date(2020, 4, 8, 0, 5, 3), "am", "AM"],
+          [new Date(2020, 4, 8, 13, 5, 3), "pm", "PM"]
+        ].forEach(data => {
+          const sut = Sut(data[0]);
 
-        expect(sut.format("a")).equal("am");
-        expect(sut.format("A")).equal("AM");
-
-        const sut2 = Sut(new Date(2020, 4, 8, 0, 5, 3));
-
-        expect(sut2.format("a")).equal("am");
-        expect(sut2.format("A")).equal("AM");
-
-        const sut3 = Sut(new Date(2020, 4, 8, 13, 5, 3));
-
-        expect(sut3.format("a")).equal("pm");
-        expect(sut3.format("A")).equal("PM");
+          expect(sut.format("a")).equal(data[1]);
+          expect(sut.format("A")).equal(data[2]);
+        });
       });
 
-      it("formats minutes", () => {
-        const sut = Sut(new Date(2020, 4, 8, 4, 5, 3));
+      it("minute", () => {
+        [
+          [new Date(2020, 4, 8, 4, 5, 3), "05", "5"],
+          [new Date(2020, 4, 8, 4, 55, 3), "55", "55"],
+          [new Date(2020, 4, 8, 13, 60, 3), "00", "0"]
+        ].forEach(data => {
+          const sut = Sut(data[0]);
 
-        expect(sut.format("mm")).equal("05");
-        expect(sut.format("m")).equal("5");
-
-        const sut2 = Sut(new Date(2020, 4, 8, 4, 55, 3));
-
-        expect(sut2.format("mm")).equal("55");
-        expect(sut2.format("m")).equal("55");
+          expect(sut.format("mm")).equal(data[1]);
+          expect(sut.format("m")).equal(data[2]);
+        });
       });
 
-      it("formats seconds", () => {
-        const sut = Sut(new Date(2020, 4, 8, 4, 5, 3));
+      it("second", () => {
+        [
+          [new Date(2020, 4, 8, 4, 5, 3), "03", "3"],
+          [new Date(2020, 4, 8, 4, 5, 10), "10", "10"],
+          [new Date(2020, 4, 8, 4, 5, 60), "00", "0"]
+        ].forEach(data => {
+          const sut = Sut(data[0]);
 
-        expect(sut.format("ss")).equal("03");
-        expect(sut.format("s")).equal("3");
-
-        const sut2 = Sut(new Date(2020, 4, 8, 4, 5, 10));
-
-        expect(sut2.format("ss")).equal("10");
-        expect(sut2.format("s")).equal("10");
+          expect(sut.format("ss")).equal(data[1]);
+          expect(sut.format("s")).equal(data[2]);
+        });
       });
 
       it("doesn't format incorrect input", () => {
@@ -290,27 +295,27 @@ describe("Mandate", () => {
       });
     });
 
-    describe("complex format", () => {
+    describe("complex", () => {
       it("formats long year and short year", () => {
         const sut = Sut(new Date(2020, 1, 1));
 
         expect(sut.format("YYYYYY")).equal("202020");
       });
 
-      it("formats two long years", () => {
+      it("two long years", () => {
         const sut = Sut(new Date(2020, 1, 1));
 
         expect(sut.format("YYYYYYYY")).equal("20202020");
       });
 
-      it("formats full date", () => {
+      it("full date", () => {
         const sut = Sut(new Date(1989, 2, 22));
 
         expect(sut.format("MMMM DD, YYYY")).equal("March 22, 1989");
       });
     });
 
-    describe("common date format", () => {
+    describe("common date", () => {
       it("formats short western date (month/day/year)", () => {
         const sut = Sut(new Date(1989, 2, 8));
 
@@ -318,27 +323,27 @@ describe("Mandate", () => {
         expect(sut.format("M/D/YY")).equal("3/8/89");
       });
 
-      it("formats long form western date month day, year", () => {
+      it("long form western date (month day, year)", () => {
         const sut = Sut(new Date(1989, 2, 8));
 
         expect(sut.format("MMMM DD, YYYY")).equal("March 08, 1989");
         expect(sut.format("MMM D, YYYY")).equal("Mar 8, 1989");
       });
 
-      it("formats ISO dates YYYY-MM-DD HH:mm:ss", () => {
+      it("ISO date (YYYY-MM-DD HH:mm:ss)", () => {
         const sut = Sut(new Date(1994, 6, 4, 14, 5, 8));
 
         expect(sut.format("YYYY-MM-DD HH:mm:ss")).equal("1994-07-04 14:05:08");
       });
 
-      it("formats common nordic date (day month year)", () => {
+      it("common euro date (day month year)", () => {
         const sut = Sut(new Date(1989, 2, 8));
 
         expect(sut.format("DD MMMM YYYY")).equal("08 March 1989");
         expect(sut.format("D MMM YYYY")).equal("8 Mar 1989");
       });
 
-      it("formats 12 hour time h:m:s a", () => {
+      it("12 hour time (h:m:s a)", () => {
         const sut = Sut(new Date(1989, 2, 8, 15, 45, 22));
 
         expect(sut.format("h:m:ss a")).equal("3:45:22 pm");
